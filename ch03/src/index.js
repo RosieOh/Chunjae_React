8caf2859d3427ecc2540e23f8ed73ab75905dd9b
RosieOh <20172207@gm.hannam.ac.kr>
2023-09-18 16:39:42 +0900

    feat : useState와 useRef의 차이, 활용법

--- ch03/src/index.js
+++ ch03/src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 import Test2 from './Test2';
 import Test3 from './Test3';
 import Test4 from './Test4';
+import Test5 from './Test5';
+
 
 // yarn으로 설치 : D:\oth\react\ch03 yarm add react-bootstrap bootstrap
 // npm으로 설치 : D:\oth\react\ch03 npm install react-bootstrap bootstrap
@@ -17,6 +19,7 @@ root.render(
     <Test2 />
     <Test3 />
     <Test4 />
+    <Test5 />
   </React.StrictMode>
 );
 
